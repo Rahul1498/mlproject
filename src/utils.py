1281df703f6f8b8a8c8a8fa32e99cc0e68aed2f0
Rahul Banik <rahulbanik1498@gmail.com>
2026-01-16 20:40:35 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,62 @@
 import os
 import sys
-import dill
-import numpy as np
+
+import numpy as np 
 import pandas as pd
-from src.exception import CustomException
-from src.logger import logging
+import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
-        import pickle
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        model_report = {}
+        report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            model_report[list(models.keys())[i]] = test_model_score
-            logging.info(f"{list(models.keys())[i]} R2 Score: {test_model_score}")
-            best_model_score = max(sorted(model_report.values()))
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-            best_model = models[best_model_name]
-            if best_model_score < 0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best Model Found: {best_model_name} with R2 Score: {best_model_score}")
-        return model_report
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
